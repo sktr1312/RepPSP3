85a34b5a767edd05cb08fad2693a2543ee58b864
Alfonso gutierrez villanueva <gutierrezalfonso30@gmail.com>
2025-02-27 20:52:01 +0100

    PRexamen-3

--- src/sockets/chat/server/ServidorChat.java
+++ src/sockets/chat/server/ServidorChat.java
@@ -1,4 +1,9 @@
 package sockets.chat.server;
 
 public class ServidorChat {
+
+public  static void main(String[] args) {
+    ServidorChatSocket servidor = new ServidorChatSocket();
+}
+
 }
--- src/sockets/chat/server/ServidorChatSocket.java
+++ src/sockets/chat/server/ServidorChatSocket.java
@@ -1,4 +1,48 @@
 package sockets.chat.server;
 
 public class ServidorChatSocket {
+
+    public void iniciar() {
+        System.out.println("Servidor de chat iniciado");
+    }
+
+    public void enviarMensagem(String mensagem) {
+        System.out.println("Mensagem enviada: " + mensagem);
+    }
+
+    public void encerrar() {
+        System.out.println("Servidor de chat encerrado");
+    }
+
+    public void receberMensagem(String mensagem) {
+        System.out.println("Mensagem recebida: " + mensagem);
+    }
+
+    public void enviarMensagemParaTodos(String mensagem) {
+        System.out.println("Mensagem enviada para todos: " + mensagem);
+    }
+
+    public void enviarMensagemPara(String mensagem, String destinatario) {
+        System.out.println("Mensagem enviada para " + destinatario + ": " + mensagem);
+    }
+
+    public void enviarMensagemParaTodosMenos(String mensagem, String remetente) {
+        System.out.println("Mensagem enviada para todos menos " + remetente + ": " + mensagem);
+    }
+
+    public void enviarMensagemParaTodosMenos(String mensagem, String remetente, String destinatario) {
+        System.out.println("Mensagem enviada para todos menos " + remetente + " e " + destinatario + ": " + mensagem);
+    }
+
+    public void enviarMensagemParaTodosMenos(String mensagem, String remetente, String destinatario1, String destinatario2) {
+        System.out.println("Mensagem enviada para todos menos " + remetente + ", " + destinatario1 + " e " + destinatario2 + ": " + mensagem);
+    }
+
+    public void enviarMensagemParaTodosMenos(String mensagem, String remetente, String destinatario1, String destinatario2, String destinatario3) {
+        System.out.println("Mensagem enviada para todos menos " + remetente + ", " + destinatario1 + ", " + destinatario2 + " e " + destinatario3 + ": " + mensagem);
+    }
+
+    public void enviarMensagemParaTodosMenos(String mensagem, String remetente, String destinatario1, String destinatario2, String destinatario3, String destinatario4) {
+        System.out.println("Mensagem enviada para todos menos " + remetente + ", " );
+    }
 }
